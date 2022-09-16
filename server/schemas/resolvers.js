434f39d416515a92d33bd95def8c4ad17143b35f
chigley22 <102197101+chigley22@users.noreply.github.com>
2022-09-16 01:30:43 -0600

    testing complete need to update ReadMe and do final push

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,34 +1,34 @@
-const { User, Book } = require("../models");
-const { signToken } = require("../utils/auth");
-const { AuthenticationError } = require("apollo-server-express");
+const { User, Book } = require('../models');
+const { signToken } = require('../utils/auth');
+const { AuthenticationError } = require('apollo-server-express');
 
 const resolvers = {
   Query: {
     me: async (parent, args, context) => {
-      if (context.user) {
-        const userData = await User.findOne({ _id: context.user._id }).select(
-          "-__v -password"
-        );
-
-        return userData;
+      if(context.user) {
+        const userData = await User.findOne({ _id: context.user._id })
+          .select('-__v -password')
+      
+      return userData;
       }
 
-      throw new AuthenticationError("Not logged in");
-    },
+      throw new AuthenticationError('Not logged in');
+    }
   },
 
   Mutation: {
+
     login: async (parent, { email, password }) => {
       const user = await User.findOne({ email });
 
       if (!user) {
-        throw new AuthenticationError("Incorrect credentials");
+        throw new AuthenticationError('Incorrect credentials');
       }
-
+      
       const correctPw = await user.isCorrectPassword(password);
 
       if (!correctPw) {
-        throw new AuthenticationError("Incorrect credentials");
+        throw new AuthenticationError('Incorrect credentials');
       }
 
       const token = signToken(user);
@@ -36,12 +36,12 @@ const resolvers = {
     },
 
     addUser: async (parent, args) => {
-      const user = await User.create(args);
-      const token = signToken(user);
-
-      return { token, user };
-    },
-
+        const user = await User.create(args);
+        const token = signToken(user);
+  
+        return { token, user };
+      },
+  
     saveBook: async (parent, { input }, context) => {
       if (context.user) {
         const updatedUser = await User.findByIdAndUpdate(
@@ -51,7 +51,7 @@ const resolvers = {
         );
         return updatedUser;
       }
-      throw new AuthenticationError("You need to be logged in!");
+      throw new AuthenticationError('You need to be logged in!')
     },
 
     removeBook: async (parent, args, context) => {
@@ -63,9 +63,9 @@ const resolvers = {
         );
         return updatedUser;
       }
-      throw new AuthenticationError("You need to be logged in!");
-    },
-  },
+      throw new AuthenticationError('You need to be logged in!')
+    } 
+  }
 };
 
 module.exports = resolvers;
